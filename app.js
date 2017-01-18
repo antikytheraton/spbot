3512000b4e979c11636aeabc117a645934de1651
JoseAngelChepo <chepo_nr@comunidad.unam.mx>
2017-01-18 01:46:44 -0600

    prueba3

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
 app.post('/webhook/', function (req, res) {
+	var senderID = event.sender.id;
     let messaging_events = req.body.entry[0].messaging
 
     for (let i = 0; i < messaging_events.length; i++) {
@@ -42,7 +43,7 @@ app.post('/webhook/', function (req, res) {
         }
         if (event.message && event.message.text == 'Hola') {
             let text = event.message.text
-            sendTextMessage(sender, "hola")
+            sendButtonMessage(senderID)
         }
         if (event.message && event.message.text == 'que') {
             let text = event.message.text
@@ -54,7 +55,6 @@ app.post('/webhook/', function (req, res) {
 //https://www.google.com.mx/maps/dir/25.6414205,-100.3220598/25.586760,-100.257281/
 //https://www.google.com.mx/maps/dir/25.6414205,-100.3220598/25.500974,-100.191265/
 
-const token = process.env.FB_PAGE_ACCESS_TOKEN_BOT
 
 function sendTextMessage(sender, text) {
     let messageData = { text:text }
@@ -74,3 +74,62 @@ function sendTextMessage(sender, text) {
         }
     })
 }
+
+function sendButtonMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "button",
+          text: "This is test text",
+          buttons:[{
+            type: "web_url",
+            url: "https://www.oculus.com/en-us/rift/",
+            title: "Open Web URL"
+          }, {
+            type: "postback",
+            title: "Trigger Postback",
+            payload: "DEVELOPER_DEFINED_PAYLOAD"
+          }, {
+            type: "phone_number",
+            title: "Call Phone Number",
+            payload: "+16505551234"
+          }]
+        }
+      }
+    }
+  };  
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      if (messageId) {
+        console.log("Successfully sent message with id %s to recipient %s", 
+          messageId, recipientId);
+      } else {
+      console.log("Successfully called Send API for recipient %s", 
+        recipientId);
+      }
+    } else {
+      console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+    }
+  });  
+}
+
+const token = process.env.FB_PAGE_ACCESS_TOKEN_BOT
