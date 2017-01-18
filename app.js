b3253a68cc47f3adf230c17a17e95ef33448680a
JoseAngelChepo <chepo_nr@comunidad.unam.mx>
2017-01-18 02:14:51 -0600

    prueba3

--- app.js
+++ app.js
@@ -85,19 +85,11 @@ function sendButtonMessage(recipientId) {
         type: "template",
         payload: {
           template_type: "button",
-          text: "This is test text",
+          text: "LLAMADA DE AUXILIO",
           buttons:[{
-            type: "web_url",
-            url: "https://www.oculus.com/en-us/rift/",
-            title: "Open Web URL"
-          }, {
-            type: "postback",
-            title: "Trigger Postback",
-            payload: "DEVELOPER_DEFINED_PAYLOAD"
-          }, {
             type: "phone_number",
             title: "Call Phone Number",
-            payload: "+16505551234"
+            payload: "5534592414"
           }]
         }
       }
