b97917dc1e719edc142b77e71b419afe07081397
JoseAngelChepo <chepo_nr@comunidad.unam.mx>
2017-01-18 01:19:21 -0600

    prueba3

--- app.js
+++ app.js
@@ -31,22 +31,35 @@ app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
 app.post('/webhook/', function (req, res) {
+	var senderID = event.sender.id;
+ 	var message = event.message;
+ 	var messageText = message.text;
+  	var messageAttachments = message.attachments;
+  	var quickReply = message.quick_reply;
+
+
     let messaging_events = req.body.entry[0].messaging
 
     for (let i = 0; i < messaging_events.length; i++) {
         let event = req.body.entry[0].messaging[i]
         let sender = event.sender.id
-        if (event.message && event.message.text == 'Hi') {
-            let text = event.message.text
-            sendTextMessage(sender, "Hi" + text.substring(0, 200))
+        if (messageText == 'Hola') {
+            let text = messageText
+            sendTextMessage(sender, "Buen día")
+            sendTextMessage(sender, "¿En qué puedo ayudarte?")
         }
-        if (event.message && event.message.text == 'Hola') {
-            let text = event.message.text
-            sendTextMessage(sender, "hola")
+        else if (messageText == 'Quiero saber mi destino') {
+            let text = messageText
+            sendTextMessage(sender, "Tu destino es el siguiente")
+            sendTextMessage(sender, "https://www.google.com.mx/maps/place/25.5534371,-100.2325287")
         }
-        if (event.message && event.message.text == 'que') {
-            let text = event.message.text
-            sendTextMessage(sender, "https://www.google.com.mx/maps/dir/25.6414205,-100.3220598/25.500974,-100.191265/")
+        else if (messageText == 'Gracias') {
+            let text = messageText
+            sendTextMessage(sender, "No hay de qué")
+        }
+        else if (messageText == 'Auxilio' || messageText == 'A' || messageText == 'a') {
+            let text = messageText
+            sendButtonMessage(senderID);
         }
     }
     res.sendStatus(200)
@@ -73,4 +86,60 @@ function sendTextMessage(sender, text) {
             console.log('Error: ', response.body.error)
         }
     })
+}
+
+function sendButtonMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "button",
+          text: "This is test text",
+          buttons:[{
+            type: "web_url",
+            url: "https://www.oculus.com/en-us/rift/",
+            title: "Open Web URL"
+          }, {
+            type: "postback",
+            title: "Trigger Postback",
+            payload: "DEVELOPER_DEFINED_PAYLOAD"
+          }, {
+            type: "phone_number",
+            title: "Call Phone Number",
+            payload: "5534592414"
+          }]
+        }
+      }
+    }
+  };  
+
+  callSendAPI(messageData);
+}
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      if (messageId) {
+        console.log("Successfully sent message with id %s to recipient %s", 
+          messageId, recipientId);
+      } else {
+      console.log("Successfully called Send API for recipient %s", 
+        recipientId);
+      }
+    } else {
+      console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+    }
+  });  
 }
