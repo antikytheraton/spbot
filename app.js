6bfb701bfc17728e5f3b12755441bd9789606239
JoseAngelChepo <chepo_nr@comunidad.unam.mx>
2017-01-18 02:51:15 -0600

    prueba3

--- app.js
+++ app.js
@@ -151,19 +151,25 @@ function sendGenericMessage(sender) {
                 "template_type": "generic",
                 "elements": [{
                     "title": "Ruta a primera zona de descanso",
+                    "subtitle": "",
                     "image_url": "/assets/ruote.png",
                     "buttons": [{
                         "type": "web_url",
                         "url": "https://www.google.com.mx/maps/dir/25.6414205,-100.3220598/25.586760,-100.257281/",
                         "title": "Mapa"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
                     }],
                 }, {
-                    "title": "Ruta a segunda zona de descanso",
-                    "image_url": "/assets/ruote.png",
+                    "title": "Ruta a primera zona de descanso",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "https://www.google.com.mx/maps/dir/25.6414205,-100.3220598/25.500974,-100.191265/",
                     "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.google.com.mx/maps/dir/25.6414205,-100.3220598/25.586760,-100.257281/",
-                        "title": "Mapa"
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
                     }],
                 }]
             }
